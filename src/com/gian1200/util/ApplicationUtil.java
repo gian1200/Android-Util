4ffc688da9d1a1829763058894679e9308d0f580
Giancarlo Calderón Cárdenas <gian1200@hotmail.com>
2013-08-23 01:22:36 -0500

    Added a printHash method to ApplicationUtil
    Helpful when you want to get the needed hash used in
    facebook.developers.com. This replace the need of using the command
    line.

--- src/com/gian1200/util/ApplicationUtil.java
+++ src/com/gian1200/util/ApplicationUtil.java
@@ -1,11 +1,20 @@
 package com.gian1200.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.util.Base64;
+import android.util.Log;
 
 public abstract class ApplicationUtil extends Application {
 
@@ -33,7 +42,30 @@ public abstract class ApplicationUtil extends Application {
 		loadData(sharedPreferences);
 	}
 
-	abstract void loadData(SharedPreferences sharedPreferences);
+	public void printHash() {
+		PackageInfo info;
+		try {
+			info = getPackageManager().getPackageInfo(getPackageName(),
+					PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md;
+				md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				String something = new String(Base64.encode(md.digest(), 0));
+				// String something = new
+				// String(Base64.encodeBytes(md.digest()));
+				Log.v("hash key", something);
+			}
+		} catch (NameNotFoundException e) {
+			Log.e("name not found", e.toString());
+		} catch (NoSuchAlgorithmException e) {
+			Log.e("no such an algorithm", e.toString());
+		} catch (Exception e) {
+			Log.e("exception", e.toString());
+		}
+	}
 
-	abstract void saveData(Editor editor);
+	protected abstract void loadData(SharedPreferences sharedPreferences);
+
+	protected abstract void saveData(Editor editor);
 }
